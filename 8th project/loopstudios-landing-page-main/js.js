cefceb95e9f9f257fec96e875f400c95c85a972f
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2021-01-27 17:25:50 +0500

    Add files via upload

--- 8th project/loopstudios-landing-page-main/js.js
+++ 8th project/loopstudios-landing-page-main/js.js
@@ -1,1 +1,15 @@
-
+const navDiv = document.querySelector("#nav-div");
+const ul = document.querySelector(".list");
+const anchor = document.querySelectorAll(".navs");
+const hamburger = document.querySelector(".hamburger");
+const cross = document.querySelector(".cross");
+hamburger.addEventListener("click", () => {
+  navDiv.classList.remove("translate");
+  hamburger.classList.add("desktop");
+  cross.classList.add("mobile");
+});
+cross.addEventListener("click", () => {
+  navDiv.classList.add("translate");
+  hamburger.classList.remove("desktop");
+  cross.classList.remove("mobile");
+});
